bed0fcde1a698bddf97146e76a624d736260be96
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-12 19:21:45 -0400

    created new components "aboutme" and "register" which are the "About Us" and "Sign Up" links in the navbar respectively. Will work on proper login/form elements for the login page before next push, but wanted to have something for everyone to see and give feedback on.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    AboutmeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
