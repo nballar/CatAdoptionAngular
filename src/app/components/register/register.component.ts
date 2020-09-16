506c1cda6ffce2548836c679f7ce5e989cc2d1ee
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-15 22:53:06 -0400

    changed a typo in my HTML document.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,15 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  // username = document.getElementById().innerHTML;
-  // password = document.getElementById().innerHTML;
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSave($event){
+
     console.log("CLICKED", $event);
   }
 }
