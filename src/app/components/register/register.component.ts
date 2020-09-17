666b4643b539821f532b389580d412bb290bd3b4
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-16 20:27:11 -0400

    adding color-changing buttons when the user hovers for accessibility.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -13,7 +13,12 @@ export class RegisterComponent implements OnInit {
   }
 
   onSave($event){
-
     console.log("CLICKED", $event);
+    console.log((document.getElementById("FirstName") as HTMLInputElement).value);
+    console.log((document.getElementById("LastName") as HTMLInputElement).value);
+    console.log((document.getElementById("inputUsername") as HTMLInputElement).value);
+    console.log((document.getElementById("userAge") as HTMLInputElement).value);
   }
 }
+
+// console.log((document.getElementById("inputPassword4") as HTMLInputElement).value);
