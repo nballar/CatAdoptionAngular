24dd810239728d11c1a978ae52b2619c35f1e21d
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-17 00:30:17 -0400

    made some styling changes: added padding to all pages, made Register inputs brown like Nancy's in the login page, chaged the text in About Us to yellow and added the brown background, added the hover-color-change to the login button. Able to log all input information to the console. Next goal: take that information, persist it to the database and send newly registered users to either the Login page or log them in immediately, to be discussed with the group.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  // What I want here is to store the user input values as the new username and password.
+  // BUUUUUT for now, it's breaking things, so I've commented it out.
+  // username = (document.getElementById("username") as HTMLInputElement).value;
+  // password = (document.getElementById("password") as HTMLInputElement).value;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSave($event){
+
+    // But hey, this part works now!!! I can log EVERYTHING without it breaking, yay!!!
     console.log("CLICKED", $event);
     console.log((document.getElementById("FirstName") as HTMLInputElement).value);
     console.log((document.getElementById("LastName") as HTMLInputElement).value);
