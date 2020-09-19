0b5da48ada1be7f5ad4bcfed0703227cac0ad336
Jasmine Edison <bigbluedanube@gmail.com>
2020-09-19 17:50:20 -0400

    registerservice and register component with HttpClient and Observables.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { RegisterService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,26 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  // What I want here is to store the user input values as the new username and password.
-  // BUUUUUT for now, it's breaking things, so I've commented it out.
-  // username = (document.getElementById("username") as HTMLInputElement).value;
-  // password = (document.getElementById("password") as HTMLInputElement).value;
+  public firstName:String;
+  public lastName:String;
+  public username:String;
+  public password:String;
+  public age:number;
 
-  constructor() { }
+  user:User;
+
+  constructor(private rs:RegisterService) { }
 
   ngOnInit(): void {
   }
 
   onSave($event){
 
-
     // But hey, this part works now!!! I can log EVERYTHING without it breaking, yay!!!
     console.log("CLICKED", $event);
+
+    this.firstName = (document.getElementById("FirstName") as HTMLInputElement).value;
+    this.lastName = (document.getElementById("FirstName") as HTMLInputElement).value;
+    this.username = (document.getElementById("inputUsername") as HTMLInputElement).value;
+    this.password = (document.getElementById("inputPassword4") as HTMLInputElement).value;
+    this.age =+ (document.getElementById("userAge") as HTMLInputElement).value;
+
+
+    let u = new User(0, this.username, this.password, this.firstName, this.lastName, null, this.age, 10, null, null)
+    this.rs.addUser(u).subscribe(
+      (response:User) =>{
+        this.user = response;
+      }
+    )
+
     console.log((document.getElementById("FirstName") as HTMLInputElement).value);
     console.log((document.getElementById("LastName") as HTMLInputElement).value);
     console.log((document.getElementById("inputUsername") as HTMLInputElement).value);
     console.log((document.getElementById("userAge") as HTMLInputElement).value);
+
   }
 }
-
-// console.log((document.getElementById("inputPassword4") as HTMLInputElement).value);
---
+++ src/app/services/register.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http:HttpClient) { }
+
+  addUser(u:User): Observable<User> {
+    let body:User = u;
+    return this.http.post<User>('http://localhost:8069/catadoption/user', body);
+  }
+}
