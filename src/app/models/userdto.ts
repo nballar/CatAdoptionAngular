70802bc99851b711d8d0d504ef5c1914cb7464d9
nballar <naballar@gmail.com>
2020-09-19 13:33:52 -0700

    login with database finished

---
+++ src/app/models/userdto.ts
@@ -0,0 +1,13 @@
+export class Userdto {
+    public username:String;
+    public password:String;
+
+    constructor(
+        username:String,
+        password:String)
+    {
+        this.username=username;
+        this.password=password;
+    }
+   
+}
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,19 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Userdto } from '../models/userdto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   authenticate(username, password) {
-    if (username === "neko" && password === "atsume") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } else {
-      return false;
-    }
+    // if (username === "neko" && password === "atsume") {
+    //   sessionStorage.setItem('username', username)
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    let ud = new Userdto(username, password);
+    return this.http.post<Userdto>("http://localhost:8069/catadoption/login", ud);
   }
 
   isUserLoggedIn() {
