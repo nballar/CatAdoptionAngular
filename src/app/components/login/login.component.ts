b6ead36853e208e307804fe8178950bc49d928b5
nballar <naballar@gmail.com>
2020-09-20 15:37:21 -0700

    login worksgit status!

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 import { Userdto } from 'src/app/models/userdto';
 import { AuthenticationService } from '../../services/authentication.service';
  
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
   username = '';
   password = '';
   invalidLogin = false;
-  ud: Userdto;
+  user: User;
 
   constructor(private router: Router,
     private loginservice: AuthenticationService) { }
@@ -23,8 +24,11 @@ export class LoginComponent implements OnInit {
  
   checkLogin() {
     this.loginservice.authenticate(this.username, this.password).subscribe(
-      (response: Userdto) => {
-        this.ud = response;
+      (response: User) => {
+        this.user = response;
+        sessionStorage.setItem('user', JSON.stringify(this.user));
+        console.log(sessionStorage);
+        this.router.navigate(['profile']);
       }
     );
   }
