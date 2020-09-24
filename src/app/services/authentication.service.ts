cd9261b5b052773277d3078beaaced7f443b7241
LizaMarie <lizamariejimenez@gmail.com>
2020-09-24 10:47:06 -0400

    watermark yay

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -14,13 +14,13 @@ export class AuthenticationService {
   authenticate(username, password): Observable<User> {
     console.log("in authenticate");
     let ud = new Userdto(username, password);
-    //return this.http.post<User>("http://localhost:8999/catadoption/login", ud) as Observable<User>;
-    return this.http.post<User>("http://ec2-18-191-238-176.us-east-2.compute.amazonaws.com:8999/catadoption/login", ud) as Observable<User>;
+    return this.http.post<User>("http://localhost:8999/catadoption/login", ud) as Observable<User>;
+    //return this.http.post<User>("http://ec2-18-191-238-176.us-east-2.compute.amazonaws.com:8999/catadoption/login", ud) as Observable<User>;
   }
 
   update(user): Observable<User> {
-    //return this.http.put<User>("http://localhost:8999/catadoption/user", user) as Observable<User>;
-    return this.http.put<User>("http://ec2-18-191-238-176.us-east-2.compute.amazonaws.com:8999/catadoption/user", user) as Observable<User>;
+    return this.http.put<User>("http://localhost:8999/catadoption/user", user) as Observable<User>;
+    //return this.http.put<User>("http://ec2-18-191-238-176.us-east-2.compute.amazonaws.com:8999/catadoption/user", user) as Observable<User>;
   }
 
   isUserLoggedIn() {
